40de33c494d88bf78d7db98d1203e589bf38830c
MinNaingKoKo <minnaingkoko2001@gmail.com>
2024-08-21 14:07:36 +0630

    save point

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,14 @@
 </script>
   
 <slot />
+
+<style lang="postcss">
+	:global(body) {
+		background-color: #fff;
+		color: #0c1523;
+		font-family: "Plus Jakarta Sans", sans-serif;
+		font-size: 18px;
+		font-weight: 400;
+		line-height: 1.28;
+	}
+</style>
