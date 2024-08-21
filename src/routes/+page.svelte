c840e05dead13aecf171b8f89d1e567fd4262af1
MinNaingKoKo <minnaingkoko2001@gmail.com>
2024-08-21 14:24:22 +0630

    save point

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+    // import logo from '$lib/images/logo.png'
+</script>
 
+<header class="pt-[50px] flex flex-row justify-center items-center">
+    <div>
+        <img src="" alt="">
+    </div>
+    <nav>
+
+    </nav>
+</header>
+<div class="bg-[url('/background.jpg')] w-[100%] h-[950px]">
+    <!-- ... -->
+</div>
 
 
